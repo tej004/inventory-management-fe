91877ace8157ef11a142514dd763f37389d36cb0
tej <jlibago09@gmail.com>
2026-01-22 14:39:44 +0800

    hotfix: root layout to redirect dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
-  );
+  redirect('/manager/dashboard');
 }
